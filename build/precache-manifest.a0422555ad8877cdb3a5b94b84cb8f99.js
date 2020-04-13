74e3a967f978e221e7912a21162ad9e2e92c1f89
Misha <gorban77@gmail.com>
2020-04-13 20:52:29 +0300

    ver5
    final

---
+++ build/precache-manifest.a0422555ad8877cdb3a5b94b84cb8f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed34b012a73e24e947db1b16caeb54f6",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "a4841de0a9592528e8f1",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c667645def4eb814ad94",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.de516b85.chunk.css"
+  },
+  {
+    "revision": "a4841de0a9592528e8f1",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.c5a99f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.c5a99f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c667645def4eb814ad94",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.570b4464.chunk.js"
+  },
+  {
+    "revision": "7ff03caac4a99a162586",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.a4acd5d5.js"
+  }
+]);
